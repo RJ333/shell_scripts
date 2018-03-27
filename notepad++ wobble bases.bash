2afe45296db7158ab8154c1a69f6d687f5b6b093
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-03-27 14:51:14 +0200

    infos zu notepad++

---
+++ notepad++ wobble bases.bash
@@ -0,0 +1,21 @@
+# andys amplicon sequencing
+# opened file in notepad++
+# crtl + f for search
+# regular expression activated
+# [] for wobble bases : Y in sequence, [CT] in notepad++ query 
+
+# check forward reads for:
+primer 1 GG[AT]AC[AT]GG[AT]TGAAC[AT]GT[AT]TA[CT]CC[CT]CC
+primer 3 CCTACGGG[ACTG]GGC[AT]GCAG
+primer 5 TGTGAATTGCA[AG][AG]AT[CT]C[AC]G
+primer 7 ATGTCACCACAAACAGAGACTAAAGC
+primer 9 ACAGGATGAACAGTTTA[CT]CC
+primer 11 GGTCAACAAATCATAAAGATATTGG
+
+# check reverse reads for
+primer 2 TAAACTTCAGGGTGACCAAA[AG]AA[CT]CA
+primer 4 GACTAC[ACT][GAC]GGGTATCTAATCC
+primer 6 CCCG[ACT][CT]TGA[CT][CT]TG[AG]GGTC[GAT]C
+primer 8 GTAAAATCAAGTCCACC[AG]CG
+primer 10 GGGTCTCCTCCTCC[ACTG]GC[ACTG]GG
+primer 12 GAAGGGTCAAAGAATGATGT
